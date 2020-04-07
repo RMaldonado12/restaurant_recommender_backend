690c668ecdf66b01f75c81eb82b07855f6530f14
actaylo <actaylo@gmail.com>
2020-04-07 17:45:00 -0500

    User profile/Friend request model

--- dinnr_project/dinnr_app/views.py
+++ dinnr_project/dinnr_app/views.py
@@ -1,33 +1,8 @@
-# from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
-# from django.contrib.auth.models import User
-
-# from rest_framework import permissions, status
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
-
-# from .serializers import UserSerializer, UserSerializerWithToken
-
-# @api_view(['GET'])
-# def current_user(request):
-#     serializer = UserSerializer(request.user)
-#     return Response(serializer.data)
-
-# class UserList(APIView):
-#     permission_classes = (permissions.AllowAny,)
-
-#     def post(self, request, format=None):
-#         serializer = UserSerializerWithToken(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 
-from dinnr_app.models import User
-from dinnr_app.serializers import UserSerializer
+from dinnr_app.models import User, UserProfile, FriendRequest
+from dinnr_app.serializers import UserSerializer, UserProfileSerializer, FriendRequestSerializer
 from dinnr_app.permissions import IsLoggedInUserOrAdmin, IsAdminUser
 
 
@@ -44,3 +19,12 @@ class UserViewSet(viewsets.ModelViewSet):
         elif self.action == 'list' or self.action == 'destroy':
             permission_classes = [IsAdminUser]
         return [permission() for permission in permission_classes]
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = UserProfileSerializer
+    queryset = UserProfile.objects.all()
+
+class FriendRequestViewSet(viewsets.ModelViewSet):
+    serializer_class = FriendRequestSerializer
+    queryset = FriendRequest.objects.all()
+    
